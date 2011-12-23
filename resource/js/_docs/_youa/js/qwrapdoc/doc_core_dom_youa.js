818f646fbbb2d5e3be04dc6ce0f808ea81eaed6c
akira <akira.cn@gmail.com>
2011-12-23 12:39:03 +0800

    merge master 1.02

--- resource/js/_docs/_youa/js/qwrapdoc/doc_core_dom_youa.js
+++ resource/js/_docs/_youa/js/qwrapdoc/doc_core_dom_youa.js
@@ -6,6 +6,7 @@
 				'namespace(sSpace,root)',
 				//'noConflict()',
 				'loadJs(url,callback)',
+				'loadJsonp(url,callback,opts)',
 				'loadCss(url)',
 				'use(moduleName,callback)',
 				'g(id)'
